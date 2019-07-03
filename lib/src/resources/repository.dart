9925b58d5916079c91859cb50fada7b4d24af847
Samfan <samfan@live.com>
2019-07-04 03:23:25 +0530

    Implement repository in resources

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,5 +1,25 @@
 import 'dart:async';
+import 'data_provider.dart';
 import '../models/model.dart';
+
+class Repository {
+  Source localDb = dataProvider;
+
+  Future<List<String>> fetchAllNames() async {
+    List<String> x = await localDb.fetchAllNames();
+    return x;
+  }
+
+  Future<Model> fetchIndividualDetail(int id) async {
+    Model x = await localDb.fetchIndividualDetail(id);
+
+    return x;
+  }
+
+  Future<int> addData(Model model) async {
+    return await localDb.addData(model);
+  }
+}
 
 abstract class Source {
   Future<List<String>> fetchAllNames();
